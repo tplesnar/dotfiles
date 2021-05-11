3eec6a1861ed69cc7599a47e95d6e844b6dc91db
Thierry Plesnar <420981+tplesnar@users.noreply.github.com>
2021-05-11 17:06:01 +0200

    init aliases

---
+++ aliases.zsh
@@ -0,0 +1,60 @@
+# Shortcuts
+alias copyssh="pbcopy < $HOME/.ssh/id_ed25519.pub"
+alias reloadshell="source $HOME/.zshrc"
+alias reloaddns="dscacheutil -flushcache && sudo killall -HUP mDNSResponder"
+alias cpwd='pwd|tr -d "\n"|pbcopy'
+alias shrug="echo '¯\_(ツ)_/¯' | pbcopy"
+alias c="clear"
+# Get macOS Software Updates, and update installed Ruby gems, Homebrew, npm, and their installed packages
+alias update='sudo softwareupdate -i -a; brew update; brew upgrade; brew cleanup; npm install npm -g; npm update -g; sudo gem update --system; sudo gem update; sudo gem cleanup'
+alias path='echo -e ${PATH//:/\\n}'
+
+# Directories
+alias dotfiles="cd $DOTFILES"
+alias library="cd $HOME/Library"
+alias bb="cd $HOME/BeepBoop"
+alias dl="cd ~/Downloads"
+alias dt="cd ~/Desktop"
+alias t="cd ~/BeepBoop/tentacle"
+
+# Laravel
+alias a="php artisan"
+alias fresh="php artisan migrate:fresh --seed"
+alias seed="php artisan db:seed"
+
+# PHP
+alias cfresh="rm -rf vendor/ composer.lock && composer i"
+alias composer="php -d memory_limit=-1 /usr/local/bin/composer"
+
+# JS
+alias nfresh="rm -rf node_modules/ package-lock.json && npm install"
+alias yfresh="rm -rf node_modules/ yarn.lock && yarn"
+alias watch="npm run watch"
+
+# Vagrant
+alias v="vagrant global-status"
+alias vup="vagrant up"
+alias vhalt="vagrant halt"
+alias vssh="vagrant ssh"
+alias vreload="vagrant reload"
+alias vrebuild="vagrant destroy --force && vagrant up"
+
+
+# Git
+alias g="git"
+alias gst="git status"
+alias gb="git branch"
+alias gc="git checkout"
+alias gl="git log --oneline --decorate --color"
+alias amend="git add . && git commit --amend --no-edit"
+alias commit="git add . && git commit -m"
+alias diff="git diff"
+alias force="git push --force"
+alias nuke="git clean -df && git reset --hard"
+alias pop="git stash pop"
+alias pull="git pull"
+alias push="git push"
+alias resolve="git add . && git commit --no-edit"
+alias stash="git stash -u"
+alias unstage="git restore --staged ."
+alias wip="commit wip"
