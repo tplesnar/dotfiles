f525795908b4915f3d406318113cc9decd63a999
Thierry Plesnar <420981+tplesnar@users.noreply.github.com>
2021-08-31 17:52:29 +0200

    Add global composer to update

--- aliases.zsh
+++ aliases.zsh
@@ -23,7 +23,7 @@ vs() {
 }
 
 # Get macOS Software Updates, and update installed Ruby gems, Homebrew, npm, and their installed packages
-alias update='sudo softwareupdate -i -a; brew update; brew upgrade; brew cleanup; npm install npm -g; npm update -g; sudo gem update --system; sudo gem update; sudo gem cleanup'
+alias update='sudo softwareupdate -i -a; brew update; brew upgrade; brew cleanup; npm install npm -g; npm update -g; sudo gem update --system; sudo gem update; sudo gem cleanup; composer global update;'
 # Clean up LaunchServices to remove duplicates in the “Open With” menu
 alias lscleanup="/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user && killall Finder"
 # Recursively delete `.DS_Store` files
