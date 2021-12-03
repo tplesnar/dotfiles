4e126b7980f96841466047ba8e5749ac2718ae68
Thierry Plesnar <420981+tplesnar@users.noreply.github.com>
2021-12-03 14:56:50 +0100

    Update path to get brew version of curl

--- path.zsh
+++ path.zsh
@@ -10,4 +10,5 @@ export PATH="node_modules/.bin:vendor/bin:$PATH"
 # Make sure coreutils are loaded before system commands
 # I've disabled this for now because I only use "ls" which is
 # referenced in my aliases.zsh file directly.
-#export PATH="$(brew --prefix coreutils)/libexec/gnubin:$PATH"
+export PATH="$(brew --prefix coreutils)/libexec/gnubin:$PATH"
+export PATH="/usr/local/opt/curl/bin:$PATH"
