e6fa814a951c6f1f7c6e94a3ee4dff69ff48fa49
Thierry Plesnar <420981+tplesnar@users.noreply.github.com>
2022-01-03 15:07:11 +0100

    Add python to path

--- path.zsh
+++ path.zsh
@@ -11,4 +11,5 @@ export PATH="node_modules/.bin:vendor/bin:$PATH"
 # I've disabled this for now because I only use "ls" which is
 # referenced in my aliases.zsh file directly.
 export PATH="$(brew --prefix coreutils)/libexec/gnubin:$PATH"
+export PATH="$(brew --prefix python)/libexec/bin:$PATH"
 export PATH="/usr/local/opt/curl/bin:$PATH"
