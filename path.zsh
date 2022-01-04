bc8de66b440c0ab95c81a17dc81c11add31b53d2
Thierry Plesnar <420981+tplesnar@users.noreply.github.com>
2022-01-04 17:50:50 +0100

    Add openJDK to path

--- path.zsh
+++ path.zsh
@@ -4,6 +4,9 @@ export PATH="$HOME/.composer/vendor/bin:$PATH"
 # Load Node global installed binaries
 export PATH="$HOME/.node/bin:$PATH"
 
+# Load openJDK java
+export PATH="/usr/local/opt/openjdk/bin:$PATH"
+
 # Use project specific binaries before global ones
 export PATH="node_modules/.bin:vendor/bin:$PATH"
 
