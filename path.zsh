72c104b99d8dee750488ea6546b315cc5b0aba25
Thierry Plesnar <420981+tplesnar@users.noreply.github.com>
2022-04-11 14:47:50 +0200

    Update path for brew

--- path.zsh
+++ path.zsh
@@ -11,8 +11,11 @@ export PATH="/usr/local/opt/openjdk/bin:$PATH"
 export PATH="node_modules/.bin:vendor/bin:$PATH"
 
 # Make sure coreutils are loaded before system commands
-# I've disabled this for now because I only use "ls" which is
-# referenced in my aliases.zsh file directly.
-export PATH="$(brew --prefix coreutils)/libexec/gnubin:$PATH"
+# Disabled for now because the can be refered with g prefix like
+# ls -> gls
+# export PATH="$(brew --prefix coreutils)/libexec/gnubin:$PATH"
 export PATH="$(brew --prefix python)/libexec/bin:$PATH"
 export PATH="/usr/local/opt/curl/bin:$PATH"
+
+#Homebrew's "sbin"
+export PATH="/usr/local/sbin:$PATH"
