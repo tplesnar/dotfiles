0dc702551ba3c2366b139df7773c941e00673156
Thierry Plesnar <420981+tplesnar@users.noreply.github.com>
2026-01-13 14:20:37 +0100

    Manage ssh differently

--- aliases.zsh
+++ aliases.zsh
@@ -1,5 +1,4 @@
 # Shortcuts
-alias copyssh="pbcopy < $HOME/.ssh/id_ed25519.pub"
 alias reloadshell="source $HOME/.zshrc"
 alias reloaddns="dscacheutil -flushcache && sudo killall -HUP mDNSResponder"
 alias ll="/opt/homebrew/opt/coreutils/libexec/gnubin/ls -AhlFo --color --group-directories-first"
